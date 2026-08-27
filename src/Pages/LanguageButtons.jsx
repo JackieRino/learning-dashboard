18fb60b2e3052e872de9afc8f5056bd67f9e4def
JackieRino <jackieosupeng@gmail.com>
2026-08-27 05:48:41 +0100

    created a learning dashboard using vite

---
+++ src/Pages/LanguageButtons.jsx
@@ -0,0 +1,18 @@
+// // import React from 'react'
+
+// export const LanguageButtons = () => {
+
+
+
+//   return (
+//     <div>
+//       <button className="projectLanguageButton">JavaScript</button>
+//         <button className="projectLanguageButton">React</button>
+//         <button className="projectLanguageButton">CSS</button>
+//         <button className="projectLanguageButton">Html</button>
+
+//     </div>
+//   )
+// }
+
+
